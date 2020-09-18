4ab1a14d4d30015d92e10d0a23e86533f0c712c5
andrew-petrenko <akk.chat@gmail.com>
2020-09-18 08:45:14 +0300

    - changed checking of instance to abstract

--- tests/AuthorizesUser.php
+++ tests/AuthorizesUser.php
@@ -2,15 +2,15 @@
 
 namespace Tests;
 
-use App\User;
 use Illuminate\Testing\TestResponse;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 trait AuthorizesUser
 {
     protected function request(string $method, string $uri, array $data = [], array $headers = []): TestResponse
     {
-        if (property_exists($this, 'authUser') && $this->authUser instanceof User) {
+        if (property_exists($this, 'authUser') && $this->authUser instanceof JWTSubject) {
             $headers = array_merge($headers, ['Authorization' => 'Bearer ' . JWTAuth::fromUser($this->authUser)]);
         }
 
